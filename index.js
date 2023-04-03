c8be7abb69e1a95a736bffa1f40deb485b2ff018
Christian-Leclerc <christianleclerc.info@gmail.com>
2023-04-03 15:38:37 -0400

    fix: issue

--- index.js
+++ index.js
@@ -21,9 +21,33 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello test test API'});
+  res.json({greeting: 'hello test test test API'});
 });
 
+app.get("/api", (req, res) =>{
+  res.json ({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString()
+  });
+});
+
+app.get("/api/:timeValue", (req, res) => {
+  const timeValue=req.params.timeValue;
+  if ( typeof Number(timeValue) === "number" && timeValue.length === 13) {
+    return res.json({
+      unix: Number(timeValue),
+      utc: new Date(Number(timeValue)).toUTCString()
+    });
+  };
+  if (new Date(timeValue).toUTCString() != "Invalid Date") {
+    return res.json({
+      unix: new Date(timeValue).getTime(),
+      utc: new Date(timeValue).toUTCString()
+    });
+  };
+  
+  res.json({ error: "Invalid Date" });
+});
 
 
 // listen for requests :)
